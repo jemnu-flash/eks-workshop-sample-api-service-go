0aaa417cad63808a0f86ea2d820bda308117ab9f
jemnu-flash <137400582+jemnu-flash@users.noreply.github.com>
2023-09-21 15:50:57 +0900

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello jemnu!!!"}
+		res := &response{Message: "Hello jemnu 3rd test!!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
